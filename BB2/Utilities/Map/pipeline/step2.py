f79b90e2d4818aac9bbb2b6091396dfb1c131655
František Horínek <frantisekhorinek@gmail.com>
2020-11-09 16:24:49 +0100

    Map processing pipeline

---
+++ BB2/Utilities/Map/pipeline/step2.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+import os
+import common
+
+def pipeline_step2():
+    source_dir = common.target_dir_step1
+    target_dir = common.target_dir_step2
+    os.makedirs(target_dir, exist_ok = True)
+
+    print("Converting data to geojson for tile %s" % common.tile_name)
+    print("  source %s" % source_dir)
+    print("  target %s" % target_dir)
+
+    #list files
+    files = os.listdir(source_dir)
+
+    for filename in files:
+        source = os.path.join(source_dir, filename)
+        layer = os.path.splitext(filename)[0].split("_")[1]
+        target_filename = os.path.splitext(filename)[0] + ".geojson"
+        target = os.path.join(target_dir, target_filename)
+
+        if os.path.exists(target):
+            print("Skipping %s, %s exists" % (filename, target_filename))
+            continue
+
+        print("Converting %s to %s" % (filename, target_filename))
+        os.system("osmtogeojson %s > %s" % (source, target))
+     
+        #invalidate next steps
+        common.invalidate_step(3, layer) 
+        
+    print("Done")
+    
+if __name__ == '__main__':  
+    pipeline_step2()
+    
---
+++ BB2/Utilities/Map/pipeline/update.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+from step1 import pipeline_step1
+from step2 import pipeline_step2
+from step3 import pipeline_step3
+from step4 import pipeline_step4
+
+pipeline_step1()
+pipeline_step2()
+pipeline_step3()
+pipeline_step4()
+
