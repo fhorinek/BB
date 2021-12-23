241954947184a3f75cb7adf59257e8686e5a9ee9
Igor Popik <igor.popik@gmail.com>
2021-12-23 21:57:41 +0100

    Revert "fixed the widget after some refactoring bug"
    This reverts commit fb0a2ebc6c2f4dc634719ed219e76d1644f5623e.

--- BB3/App/gui/widgets/types/widget_toff_dist.c
+++ BB3/App/gui/widgets/types/widget_toff_dist.c
@@ -44,12 +44,13 @@ static void DToff_update(widget_slot_t * slot)
 
     if (fc.flight.takeoff_distance != INVALID_UINT32)
     {
+		format_distance(value, fc.flight.takeoff_distance);
+
 		if (local->units != NULL)
 		{
 			format_distance_units(value, fc.flight.takeoff_bearing);
 			lv_label_set_text(local->units, value);
 		}
-		format_distance(value, fc.flight.takeoff_distance);
     }
     else
     {
