c189efccc85f39b4f6cbe1ff729878812f5c71ab
Dr. Tilmann Bubeck <tilmann@bubecks.de>
2022-02-09 19:45:39 +0100

    Widget Menu on "map" to change zoom level.

--- BB3/App/gui/widgets/types/widget_map.c
+++ BB3/App/gui/widgets/types/widget_map.c
@@ -1,5 +1,5 @@
 /*
- * widget_flight_time.c
+ * widget_map.c
  *
  *  Created on: 11. 8. 2020
  *      Author: horinek
@@ -12,7 +12,7 @@
 
 #include "etc/geo_calc.h"
 
-REGISTER_WIDGET_IU(Map,
+REGISTER_WIDGET_ISUE(Map,
     "Map",
     WIDGET_MIN_W,
     WIDGET_MIN_H,
@@ -25,6 +25,10 @@ REGISTER_WIDGET_IU(Map,
     lv_point_t points[WIDGET_ARROW_POINTS];
     lv_obj_t * poi[NUMBER_OF_POI];
     uint8_t poi_magic[NUMBER_OF_POI];
+
+    lv_obj_t * edit;
+    uint32_t last_action;
+    uint8_t action_cnt;
 
     uint8_t magic;
 );
@@ -44,6 +48,10 @@ static void Map_init(lv_obj_t * base, widget_slot_t * slot)
     widget_create_base(base, slot);
 
     local->magic = 0xFF;
+
+    local->edit = NULL;
+    local->last_action = 0;
+    local->action_cnt = 0;
 
     for (uint8_t i = 0; i < 9; i++)
     {
@@ -192,6 +200,89 @@ static void Map_update(widget_slot_t * slot)
 			lv_obj_set_hidden(local->arrow, true);
 			lv_obj_set_hidden(local->dot, false);
     	}
+    }
+
+    if (local->edit != NULL)
+    {
+    	lv_obj_t * base = widget_edit_overlay_get_base(local->edit);
+    	lv_obj_t * zoom = lv_obj_get_child(base, NULL);
+
+    	char buff[32];
+
+    	snprintf(buff, sizeof(buff), "%d", config_get_int(&profile.map.zoom_flight));
+    	lv_label_set_text(zoom, buff);
+    }
+}
+
+static void Map_stop(widget_slot_t * slot)
+{
+}
+
+static void Map_edit(widget_slot_t * slot, uint8_t action)
+{
+    if (action == WIDGET_ACTION_CLOSE)
+    {
+        if (local->edit != NULL)
+        {
+            widget_destroy_edit_overlay(local->edit);
+            local->edit = NULL;
+        }
+
+        return;
+    }
+
+    if (action == WIDGET_ACTION_MIDDLE)
+    {
+    	widget_destroy_edit_overlay(local->edit);
+    	local->edit = NULL;
+    }
+
+    if (action == WIDGET_ACTION_LEFT || action == WIDGET_ACTION_RIGHT || action == WIDGET_ACTION_HOLD)
+    {
+		if (local->edit == NULL)
+		{
+			//create menu
+			local->edit = widget_create_edit_overlay("Map", "Set Zoom Level");
+			lv_obj_t * base = widget_edit_overlay_get_base(local->edit);
+			lv_obj_t * zoom = lv_label_create(base, NULL);
+			lv_obj_set_style_local_text_font(zoom, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, gui.styles.widget_fonts[FONT_XL]);
+
+			//update values
+			Map_update(slot);
+		}
+
+		if (HAL_GetTick() - local->last_action < 300)
+		{
+			if (local->action_cnt < 50)
+				local->action_cnt++;
+		}
+		else
+		{
+			local->action_cnt = 0;;
+		}
+
+		//INFO("AC %u %lu", local->action_cnt, HAL_GetTick() - local->last_action);
+
+		local->last_action = HAL_GetTick();
+
+		int8_t diff = 0;
+		if (action == WIDGET_ACTION_LEFT)
+			diff = -1;
+
+		if (action == WIDGET_ACTION_RIGHT)
+			diff = 1;
+
+		if (diff != 0)
+		{
+			diff *= 1 + (local->action_cnt / 5);
+
+			int16_t zoom = config_get_int(&profile.map.zoom_flight);
+                        int16_t new = zoom + diff;
+			config_set_int(&profile.map.zoom_flight, new);
+
+			widget_reset_edit_overlay_timer();
+		}
+
     }
 
 }
