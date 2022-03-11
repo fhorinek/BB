c1a3bf28ed4f22b97e2e07273c4d82e67e08d31a
František Horínek <frantisekhorinek@gmail.com>
2022-03-12 00:52:20 +0100

    Fix issues in release configurations

--- BB3/Core/Src/main.c
+++ BB3/Core/Src/main.c
@@ -284,16 +284,6 @@ void MPU_Config(void)
   MPU_InitStruct.IsBufferable = MPU_ACCESS_NOT_BUFFERABLE;
 
   HAL_MPU_ConfigRegion(&MPU_InitStruct);
-
-  /** Initializes and configures the Region and the memory to be protected
-  */
-  MPU_InitStruct.Number = MPU_REGION_NUMBER1;
-  MPU_InitStruct.BaseAddress = 0x24000000;
-  MPU_InitStruct.Size = MPU_REGION_SIZE_1MB;
-  MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL1;
-//  MPU_InitStruct.IsBufferable = MPU_ACCESS_BUFFERABLE;
-
-  HAL_MPU_ConfigRegion(&MPU_InitStruct);
   /* Enables the MPU */
   HAL_MPU_Enable(MPU_PRIVILEGED_DEFAULT);
 
