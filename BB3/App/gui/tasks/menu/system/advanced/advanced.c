7615410ea8d8eaeca990c65eebf0557d49e8007f
František Horínek <frantisekhorinek@gmail.com>
2022-03-16 09:35:55 +0100

    Update lsm9ds1.c and advanced.c

--- BB3/App/gui/tasks/menu/system/advanced/advanced.c
+++ BB3/App/gui/tasks/menu/system/advanced/advanced.c
@@ -108,6 +108,28 @@ static bool clear_debug_file_cb(lv_obj_t * obj, lv_event_t event)
     return true;
 }
 
+static void advanced_crash_dialog_cb(uint8_t res, void * data)
+{
+    if (res == dialog_res_yes)
+    {
+        config_set_bool(&config.debug.crash_dump, true);
+        bsod_msg("Creating diagnostic report");
+    }
+}
+
+
+static bool advanced_crash_cb(lv_obj_t * obj, lv_event_t event)
+{
+    if (event == LV_EVENT_CLICKED)
+    {
+        dialog_show("Confirm", "This will stop the device and create diagnostic report.\n\n"
+                "We will trigger crash to generate the report.\n\n"
+                "The crash report will be available on your strato via USB massstorage.", dialog_yes_no, advanced_crash_dialog_cb);
+    }
+
+    return true;
+}
+
 lv_obj_t * advanced_init(lv_obj_t * par)
 {
     lv_obj_t * list = gui_list_create(par, "Advanced settings", &gui_system, NULL);
@@ -127,6 +149,7 @@ lv_obj_t * advanced_init(lv_obj_t * par)
 		gui_list_auto_entry(list, "Reset calibration", CUSTOM_CB, advanced_calib_clear);
 	}
 
+    gui_list_auto_entry(list, "Create diagnostic report", CUSTOM_CB, advanced_crash_cb);
     gui_list_auto_entry(list, "Crash reports", &config.debug.crash_dump, NULL);
     gui_list_auto_entry(list, "Debug to file", &config.debug.use_file, NULL);
     gui_list_auto_entry(list, "Clear debug.log", CUSTOM_CB, clear_debug_file_cb);
