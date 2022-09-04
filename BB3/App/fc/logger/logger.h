a70635661d775a08d54deb6a2ab4745941fa7f65
Dr. Tilmann Bubeck <tilmann@bubecks.de>
2022-09-04 12:02:06 +0200

    First version of flightbook

--- BB3/App/fc/logger/logger.h
+++ BB3/App/fc/logger/logger.h
@@ -11,10 +11,30 @@
 #include "common.h"
 #include "fc/fc.h"
 
+#define FS_NO_DATA 0
+
+/**
+ * A structure holding all statistics of a flight stored in a log file.
+ */
+typedef struct flight_stats
+{
+	uint32_t start_time;    // in seconds since epoch UTC
+	uint32_t duration;      // in seconds
+	uint32_t odo;           // in cm
+
+	int16_t max_alt;        // in m
+	int16_t min_alt;        // in m
+
+	int16_t max_climb;      // in cm/s
+	int16_t max_sink;       // in cm/s
+} flight_stats_t;
+
 void logger_init();
 void logger_start();
 void logger_comment(const char *format, ...);
 void logger_stop();
+void logger_write_flight_stats(flight_stats_t f_stat);
+void logger_read_flight_stats(const char *filename, flight_stats_t *f_stat);
 
 fc_logger_status_t logger_state();
 
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # SkyBean Strato
 
+TEST please do not merge
+
 This is offical source code repositary for SkyBean Strato
 
 * [Firmware repo](https://strato.skybean.eu/fw/)
