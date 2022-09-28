924b72dc2b772395b60965e5015d465752adda60
Dr. Tilmann Bubeck <tilmann@bubecks.de>
2022-09-28 18:22:06 +0200

    Too many outputs on some cases. System not starting anymore.

--- BB3/App/drivers/sd.c
+++ BB3/App/drivers/sd.c
@@ -140,19 +140,19 @@ uint8_t sd_write_blocks(uint32_t *pData, uint32_t WriteAddr, uint32_t NumOfBlock
 
 void HAL_SD_AbortCallback(SD_HandleTypeDef *hsd)
 {
-	INFO("Abort");
+	DBG("Abort");
     osSemaphoreRelease(sd_dma_semaphore);
 }
 
 void HAL_SD_TxCpltCallback(SD_HandleTypeDef *hsd)
 {
-	INFO("TxC");
+	DBG("TxC");
     osSemaphoreRelease(sd_dma_semaphore);
 }
 
 void HAL_SD_RxCpltCallback(SD_HandleTypeDef *hsd)
 {
-	INFO("TxR");
+	DBG("TxR");
     osSemaphoreRelease(sd_dma_semaphore);
 }
 
